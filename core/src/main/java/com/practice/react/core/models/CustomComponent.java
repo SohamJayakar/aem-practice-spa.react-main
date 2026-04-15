0c5681f366fc9aaae0f27ee7740a2de4ffa427e2
SohamJayakar <sohamjayakar@gmail.com>
2026-04-15 16:56:57 +0530

    Initial Commit

---
+++ core/src/main/java/com/practice/react/core/models/CustomComponent.java
@@ -0,0 +1,9 @@
+package com.practice.react.core.models;
+
+import com.adobe.cq.export.json.ComponentExporter;
+
+public interface CustomComponent extends ComponentExporter{
+
+    public String getMessage();
+
+}
